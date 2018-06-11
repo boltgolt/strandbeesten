8eb4f4e6c2a9252026e6ae89acc7bda2bed6e579
boltgolt <boltgolt@gmail.com>
2018-06-11 15:15:28 +0200

    DNA encoding and decoding fully working

---
+++ dna.js
@@ -0,0 +1,117 @@
+/**
+ * Decode a hex DNA string
+ * @param  {string} hex The encoded DNA hex
+ * @return {object}     A DNA object
+ */
+function decodeDNA(hex) {
+	// Create an empty DNA object
+	let dna = {
+		motor: {},
+		legs: []
+	}
+	// Create an ampty binary string
+	let bin = ""
+
+	// First of all, we need to parse the hex to the source binary string
+	for (let hexChar of hex) {
+		// Take each hex char and transform it to a 4-bit padded string
+		bin += parseInt(hexChar, 16).toString(2).padStart(4, "0")
+	}
+
+	// Check if the header is correct
+	if (bin.substr(0, 1) != "1") {
+		throw "Invalid DNA header"
+	}
+
+	// Contains the XORed checksum result
+	let checksum = 0
+
+	// Go through char by char
+	for (let i in bin) {
+		// Skip the checksum bit itself
+		if (i == 127) break;
+
+		// XOR the found bit to the checksum
+		checksum ^= parseInt(bin[i])
+	}
+
+	// Compare the found checksum with the calculated one
+	if (bin.substr(127, 1) != checksum) {
+		throw "Invalid DNA checksum"
+	}
+
+	// Height from the top of the frame to the ground
+	// START 1		LENGTH 10		END	10
+	dna.groundHeight = parseInt(bin.substr(1, 10), 2) / 10
+
+
+	// The radius of the motor arm
+	// START 11		LENGTH 8		END	18
+	dna.motor.radius = parseInt(bin.substr(11, 8), 2) / 10
+
+	// The speed of the motor rotation
+	// START 19		LENGTH 8		END	26
+	dna.motor.speed = 128 - parseInt(bin.substr(19, 8), 2)
+
+	// Length of every pipe
+	// The minimum length of a leg is 10, so 10 is added to the total here
+	// START 27		LENGTH 10		END	127
+	for (let i = 0; i <= 9; i++) {
+		dna.legs.push(10 + parseInt(bin.substr(27 + (10 * i), 10), 2) / 10)
+	}
+
+	// Return the filled DNA object
+	return dna
+}
+
+/**
+ * Encode a DNA object to a hex string
+ * @param  {object} dna The DNA object to encode
+ * @return {string}     An encoded hex string
+ */
+function encodeDNA(dna) {
+	// Start the binary string off with the header
+	let bin = "1"
+
+	// Add the 10-bit ground height
+	// Devided by 10 to allow for 1 decimal
+	bin += Math.round(dna.groundHeight * 10).toString(2).padStart(10, "0")
+
+	// Add the 8-bit motor radius
+	// Has also been devided by 10 for 1 decimal
+	bin += Math.round(dna.motor.radius * 10).toString(2).padStart(8, "0")
+
+	// Add the 8-bit motor speed
+	// Had 128 subtracted to allow for negative numbers
+	bin += Math.round(-dna.motor.speed + 128).toString(2).padStart(8, "0")
+
+	// Go through each leg
+	for (let leg of dna.legs) {
+		// Add the 10-bit leg length to the binary
+		// Had a padding of 10 for minimal leg length
+		bin += Math.round((leg - 10) * 10).toString(2).padStart(10, "0")
+	}
+
+	// Start with an empty checksum
+	let checksum = 0
+
+	// Go through the binary string
+	for (let bit of bin) {
+		// Calculate the XOR for every bit and add it
+		checksum ^= parseInt(bit)
+	}
+
+	// Add the checksum to the binary, completing it
+	bin += checksum
+	// Create an empty hex
+	let hex = ""
+
+	// Go though the binary string in steps of 4
+	for (let i = 0; i < bin.length; i += 4) {
+		// Convert every 4 bits to a hex character and add it to the hex
+		hex += parseInt(bin.substr(i, 4), 2).toString(16)
+	}
+
+	// Return the completed hex
+	return hex
+}
--- main.js
+++ main.js
@@ -13,229 +13,247 @@ const World = Matter.World
 const Bodies = Matter.Bodies
 const Vector = Matter.Vector
 
-// Create physiscs engine
-let engine = Engine.create()
-// Create a world
-let world = engine.world
+let group
 
-// Set the gravity in this world
-world.gravity.y = 2;
-world.gravity.scale = 0.002;
+function startSimulation(dnaString, updateCallback, completedCallback) {
+	// Get the options fromt the DNA
+	const dna = decodeDNA(dnaString)
 
-// Create the renderer
-var render = Render.create({
-	// Add the canvas element
-	element: document.body,
-	// Set the created engine
-	engine: engine,
-	options: {
-		width: 800,
-		height: 600,
-		wireframes: false,
-		background: "#4186E0"
-	}
-})
+	// Create physiscs engine
+	let engine = Engine.create()
+	// Create a world
+	let world = engine.world
 
-// Start the renderer
-Render.run(render)
+	// Set the gravity in this world
+	world.gravity.y = 2;
+	world.gravity.scale = 0.002;
 
-// Create the runner and start it
-let runner = Runner.create()
-Runner.run(runner, engine)
-
-// How far we are in the circle engine loop
-let loop = 0
-// The length of the loop
-let loopLength = 100
-// The radius of the circle arm
-let radius = 15
-// If the grace period has ended
-let gracePeriod = true
-// The hight of the ground in pixels from the top
-let goundHeight = 193
-
-// Run every tick
-Events.on(runner, "tick", function() {
-	// Set the X and Y of the motor link to the right frame in the loop
-	motorLink.body.pointA.x = 338 + Math.sin((loop / loopLength) * Math.PI * 2) * radius
-	motorLink.body.pointA.y = 92.2 + Math.sin(((loop / loopLength) + 0.25) * Math.PI * 2) * radius
-
-	// If the grace period has ended
-	if (!gracePeriod) {
-		// Let the top helper link move with the top pipe
-		topLink.body.pointA.x = 300 + Math.sin((loop / loopLength) * Math.PI * 2) * radius * 2
-
-		// Increment the loop
-		loop++
-		// Reset the loop if we've het the max length
-		if (loop >= loopLength) loop = 0
-	}
-
-	let pipeLength = 65.7
-
-	// Calculate the Y diff with some hardcore math
-	let y = pipeLength / 2 * Math.sin(diagonalBottomPipe.body.angle)
-	// Do the same for the X diff but with pythagoras
-	let x = Math.sqrt(Math.pow(pipeLength / 2, 2) - Math.pow(y, 2))
-
-	// If the diagonal is to the right of the straigt bottom pipe,
-	// reverse the X so it doesn't float on the wrong side
-	if (rightBottomPipe.body.position.x < diagonalBottomPipe.body.position.x) {
-		x *= -1
-	}
-
-	// Check if the leg is touching the ground
-	if (diagonalBottomPipe.body.position.y - y > goundHeight - 3) {
-		// If it is, let the ground move with the leg for this frame by applying a diff
-		Body.setPosition(ground, {
-			x: ground.position.x - (frictionCircle.position.x - (diagonalBottomPipe.body.position.x + x)),
-			y: goundHeight + 100
-		})
-
-		// Mark the foot as touching
-		frictionCircle.render.fillStyle = "#0f0"
-	}
-	else {
-		// Mark the foot as floating
-		frictionCircle.render.fillStyle = "#f00"
-	}
-
-	// Position the circle at the end of the foot
-	Body.setPosition(frictionCircle, {
-		x: diagonalBottomPipe.body.position.x + x,
-		y: diagonalBottomPipe.body.position.y - y
-	})
-})
-
-// Group in which collisions will be disabled
-let group = Body.nextGroup(true)
-
-// Init empty arrays to fill with objects
-let pipes = []
-let links = []
-
-// Top triangle
-//  Diagonal	[0]
-pipes.push(new Pipe(350, 100, 55.8))
-//  Bottom		[1]
-pipes.push(new Pipe(350, 120, 40.1))
-//  Right		[2]
-pipes.push(new Pipe(440, 160, 41.5))
-
-// Link top triangle
-links.push(new Link(pipes[0], pipes[1], 1, [-1, 1]))
-links.push(new Link(pipes[1], pipes[2], 1, [-1, 1]))
-links.push(new Link(pipes[2], pipes[0], 1, [-1, 1]))
-
-// Square connectors
-//  Right		[3]
-pipes.push(new Pipe(340, 190, 39.4))
-//  Left		[4]
-pipes.push(new Pipe(390, 190, 39.4))
-
-// Square to triangle connectors
-links.push(new Link(pipes[1], pipes[3], 1, [-1, 1]))
-links.push(new Link(pipes[2], pipes[4], 1, [-1, 1]))
-
-// Bottom triangle
-//  Top			[5]
-pipes.push(new Pipe(350, 220, 36.7))
-//  Right		[6]
-let rightBottomPipe = new Pipe(440, 220, 49.0)
-pipes.push(rightBottomPipe)
-//  Diagonal	[7]
-let diagonalBottomPipe = new Pipe(340, 100, 65.7)
-pipes.push(diagonalBottomPipe)
-
-// Link bottom triangle to square
-links.push(new Link(pipes[3], pipes[5], 1, [-1, 1]))
-links.push(new Link(pipes[5], pipes[4], 1, [-1, -1]))
-
-// Link bottom triangle together
-links.push(new Link(pipes[5], pipes[6], 1, [1, 1]))
-links.push(new Link(pipes[5], pipes[7], 1, [-1, 1]))
-links.push(new Link(pipes[6], pipes[7], 1, [-1, -1]))
-
-// Right motor connectors
-//  Top			[8]
-pipes.push(new Pipe(440, 220, 50))
-//  bottom		[9]
-pipes.push(new Pipe(340, 100, 61.9))
-
-// Connect driving right motor
-links.push(new Link(pipes[8], pipes[0], 1, [-1, -1]))
-links.push(new Link(pipes[9], pipes[5], 1, [-1, 1]))
-links.push(new Link(pipes[8], pipes[9], 1, [1, 1]))
-
-// Link at the top of the leg to keep it up straight
-var topLink = new Link([260, 0], pipes[1], 0.1, [1, 0.8])
-links.push(topLink)
-
-// Link the static point in the top triangle
-links.push(new Link([300, 100], pipes[2], 1))
-
-// Link the motor to the connectors
-var motorLink = new Link([338, 92.2], pipes[9], 1)
-links.push(motorLink)
-
-// Empty array for only the physiscs bodies
-let bodies = []
-
-// Go through every link and add its body
-for (let link of links) {
-	bodies.push(link.body)
-}
-// Do the same for the pipes
-for (let pipe of pipes) {
-	bodies.push(pipe.body)
-}
-
-// Add them all to the world
-World.add(world, bodies)
-
-// Create circle to show if the leg is touching the ground
-let frictionCircle = Bodies.circle(100, 100, 5, {
-	isStatic: true,
-	collisionFilter: {
-		group: group
-	},
-	render: {
-		fillStyle: "#f00",
-		opacity: 0.4
-	}
-})
-
-// Add it to the world
-World.add(world, frictionCircle)
-
-// Create the ground with the green texture
-var ground = Bodies.rectangle(300, 400, 3600, 200, {
-	isStatic: true,
-	render: {
-		sprite: {
-			texture: "ground.png"
+	// Create the renderer
+	var render = Render.create({
+		// Add the canvas element
+		element: document.body,
+		// Set the created engine
+		engine: engine,
+		options: {
+			width: 800,
+			height: 600,
+			wireframes: false,
+			background: "#4186E0"
 		}
+	})
+
+	// Start the renderer
+	Render.run(render)
+
+	// Create the runner and start it
+	let runner = Runner.create()
+	Runner.run(runner, engine)
+
+	// How far we are in the circle engine loop
+	let loop = 0
+	// The length of the loop
+	let loopLength = 100
+	// The radius of the circle arm
+	let radius = 15
+	// If the grace period has ended
+	let gracePeriod = true
+	// The hight of the ground in pixels from the top
+	let goundHeight = 193
+
+	// Run every tick
+	Events.on(runner, "tick", function() {
+		// Set the X and Y of the motor link to the right frame in the loop
+		motorLink.body.pointA.x = 338 + Math.sin((loop / loopLength) * Math.PI * 2) * radius
+		motorLink.body.pointA.y = 92.2 + Math.sin(((loop / loopLength) + 0.25) * Math.PI * 2) * radius
+
+		// If the grace period has ended
+		if (!gracePeriod) {
+			// Let the top helper link move with the top pipe
+			topLink.body.pointA.x = 300 + Math.sin((loop / loopLength) * Math.PI * 2) * radius * 2
+
+			// Increment the loop
+			loop++
+			// Reset the loop if we've het the max length
+			if (loop >= loopLength) loop = 0
+		}
+
+		let pipeLength = 65.7
+
+		// Calculate the Y diff with some hardcore math
+		let y = pipeLength / 2 * Math.sin(diagonalBottomPipe.body.angle)
+		// Do the same for the X diff but with pythagoras
+		let x = Math.sqrt(Math.pow(pipeLength / 2, 2) - Math.pow(y, 2))
+
+		// If the diagonal is to the right of the straigt bottom pipe,
+		// reverse the X so it doesn't float on the wrong side
+		if (rightBottomPipe.body.position.x < diagonalBottomPipe.body.position.x) {
+			x *= -1
+		}
+
+		// Check if the leg is touching the ground
+		if (diagonalBottomPipe.body.position.y - y > goundHeight - 3) {
+			// If it is, let the ground move with the leg for this frame by applying a diff
+			Body.setPosition(ground, {
+				x: ground.position.x - (frictionCircle.position.x - (diagonalBottomPipe.body.position.x + x)),
+				y: goundHeight + 100
+			})
+
+			// Mark the foot as touching
+			frictionCircle.render.fillStyle = "#0f0"
+		}
+		else {
+			// Mark the foot as floating
+			frictionCircle.render.fillStyle = "#f00"
+		}
+
+		// Position the circle at the end of the foot
+		Body.setPosition(frictionCircle, {
+			x: diagonalBottomPipe.body.position.x + x,
+			y: diagonalBottomPipe.body.position.y - y
+		})
+	})
+
+	// Group in which collisions will be disabled
+	group = Body.nextGroup(true)
+
+	// Init empty arrays to fill with objects
+	let pipes = []
+	let links = []
+
+	// Top triangle
+	//  Diagonal	[0]
+	pipes.push(new Pipe(350, 100, 55.8))
+	//  Bottom		[1]
+	pipes.push(new Pipe(350, 120, 40.1))
+	//  Right		[2]
+	pipes.push(new Pipe(440, 160, 41.5))
+
+	// Link top triangle
+	links.push(new Link(pipes[0], pipes[1], 1, [-1, 1]))
+	links.push(new Link(pipes[1], pipes[2], 1, [-1, 1]))
+	links.push(new Link(pipes[2], pipes[0], 1, [-1, 1]))
+
+	// Square connectors
+	//  Right		[3]
+	pipes.push(new Pipe(340, 190, 39.4))
+	//  Left		[4]
+	pipes.push(new Pipe(390, 190, 39.4))
+
+	// Square to triangle connectors
+	links.push(new Link(pipes[1], pipes[3], 1, [-1, 1]))
+	links.push(new Link(pipes[2], pipes[4], 1, [-1, 1]))
+
+	// Bottom triangle
+	//  Top			[5]
+	pipes.push(new Pipe(350, 220, 36.7))
+	//  Right		[6]
+	let rightBottomPipe = new Pipe(440, 220, 49.0)
+	pipes.push(rightBottomPipe)
+	//  Diagonal	[7]
+	let diagonalBottomPipe = new Pipe(340, 100, 65.7)
+	pipes.push(diagonalBottomPipe)
+
+	// Link bottom triangle to square
+	links.push(new Link(pipes[3], pipes[5], 1, [-1, 1]))
+	links.push(new Link(pipes[5], pipes[4], 1, [-1, -1]))
+
+	// Link bottom triangle together
+	links.push(new Link(pipes[5], pipes[6], 1, [1, 1]))
+	links.push(new Link(pipes[5], pipes[7], 1, [-1, 1]))
+	links.push(new Link(pipes[6], pipes[7], 1, [-1, -1]))
+
+	// Right motor connectors
+	//  Top			[8]
+	pipes.push(new Pipe(440, 220, 50))
+	//  bottom		[9]
+	pipes.push(new Pipe(340, 100, 61.9))
+
+	// Connect driving right motor
+	links.push(new Link(pipes[8], pipes[0], 1, [-1, -1]))
+	links.push(new Link(pipes[9], pipes[5], 1, [-1, 1]))
+	links.push(new Link(pipes[8], pipes[9], 1, [1, 1]))
+
+	// Link at the top of the leg to keep it up straight
+	var topLink = new Link([260, 0], pipes[1], 0.1, [1, 0.8])
+	links.push(topLink)
+
+	// Link the static point in the top triangle
+	links.push(new Link([300, 100], pipes[2], 1))
+
+	// Link the motor to the connectors
+	var motorLink = new Link([338, 92.2], pipes[9], 1)
+	links.push(motorLink)
+
+	// Empty array for only the physiscs bodies
+	let bodies = []
+
+	// Go through every link and add its body
+	for (let link of links) {
+		bodies.push(link.body)
 	}
-})
+	// Do the same for the pipes
+	for (let pipe of pipes) {
+		bodies.push(pipe.body)
+	}
 
-// Add that to the world too
-World.add(world, ground)
+	// Add them all to the world
+	World.add(world, bodies)
+
+	// Create circle to show if the leg is touching the ground
+	let frictionCircle = Bodies.circle(100, 100, 5, {
+		isStatic: true,
+		collisionFilter: {
+			group: group
+		},
+		render: {
+			fillStyle: "#f00",
+			opacity: 0.4
+		}
+	})
+
+	// Add it to the world
+	World.add(world, frictionCircle)
+
+	// Create the ground with the green texture
+	var ground = Bodies.rectangle(300, 400, 3600, 200, {
+		isStatic: true,
+		render: {
+			sprite: {
+				texture: "ground.png"
+			}
+		}
+	})
+
+	// Add that to the world too
+	World.add(world, ground)
 
 
-// Change the camera to keep the leg in the middle
-Render.lookAt(render, {
-	min: {x: 0, y: 0},
-	max: {x: 600, y: 300}
-})
+	// Change the camera to keep the leg in the middle
+	Render.lookAt(render, {
+		min: {x: 0, y: 0},
+		max: {x: 600, y: 300}
+	})
 
-// Grace period timer
-// The first few seconds the leg can flop about all it wants, but after
-// that these more realistic settings come into affect
-setTimeout(function() {
-	// Set the gravity less strongly
-	world.gravity.y = 1.5
-	// Position the ground at the requested height
-	Body.setPosition(ground, {x: 300, y: goundHeight + 100})
-	// Enable the helping top link and motor loop
-	gracePeriod = false
-}, 2000)
+	// Grace period timer
+	// The first few seconds the leg can flop about all it wants, but after
+	// that these more realistic settings come into affect
+	setTimeout(function() {
+		// Set the gravity less strongly
+		world.gravity.y = 1.5
+		// Position the ground at the requested height
+		Body.setPosition(ground, {x: 300, y: goundHeight + 100})
+		// Enable the helping top link and motor loop
+		gracePeriod = false
+	}, 2000)
+}
+
+// startSimulation()
+//
+
+let ins ="bfcfcfcff3fcfffffffffffffffffffe"
+let encoded = decodeDNA(ins)
+
+console.log(JSON.stringify(encoded))
+
+console.log(encodeDNA(encoded))
+console.log(ins)
